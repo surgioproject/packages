93eba4557f4b14aa77f6956d754ff05f0629ec80
Roy Li <me@dada.li>
2020-03-09 18:12:09 +0800

    feat: embed 模式的 artifact 页面

---
+++ packages/gateway-frontend/src/hooks/useNavElements.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  useLocation,
+} from 'react-router-dom';
+import { BAREBONE_PAGES } from '../libs/constant';
+
+export default function useNavElements() {
+  const location = useLocation();
+  const result = BAREBONE_PAGES.some(page => page.test(location.pathname));
+
+  return !result;
+}
---
+++ packages/gateway-frontend/src/libs/constant.ts
@@ -0,0 +1,3 @@
+export const BAREBONE_PAGES: ReadonlyArray<RegExp> = [
+  /^\/embed\/.*$/,
+];
