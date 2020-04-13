b0a16cbb465ab6dafeea6ab6670d1563e2220791
Roy Li <me@dada.li>
2020-04-13 17:06:16 +0800

    refactor: 使用官方推荐的方式启动 serverless 服务
    Doc: https://www.fastify.io/docs/latest/Serverless/#zeit-now

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -1,28 +1,18 @@
 import { NowRequest, NowResponse } from '@now/node/dist';
-import { ServerFactoryFunction, ServerFactoryHandlerFunction } from 'fastify';
-import { createServer } from 'http';
 import { bootstrap } from './bootstrap';
 
-export const createHttpServer = (): any => {
-  let nowHandler: ServerFactoryHandlerFunction;
-  const serverFactory: ServerFactoryFunction = handler => {
-    nowHandler = (req, res) => {
-      handler(req, res);
-    };
-    return createServer(nowHandler);
-  };
-  const server = bootstrap({
-    serverFactory,
-  })
+export const createHttpServer = (): ((req: NowRequest, res: NowResponse) => Promise<void>) => {
+  const ready = bootstrap()
     .then(app => {
       return app.init()
         .then(() => app.getHttpAdapter().getInstance().ready())
+        .then(() => app);
     });
 
   return async (req: NowRequest, res: NowResponse) => {
-    await server;
+    const app = await ready;
 
-    return nowHandler(req, res);
+    app.getHttpAdapter().getInstance().server.emit('request', req, res);
   };
 };
 
