769d01cb2738c14322951cd31b2e629e2bda5cf5
Roy Li <me@dada.li>
2020-04-17 20:51:15 +0800

    revert: 关闭新启动方式开关

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -2,8 +2,6 @@ import { NowRequest, NowResponse } from '@now/node/dist';
 import { bootstrap } from './bootstrap';
 
 export const createHttpServer = (): ((req: NowRequest, res: NowResponse) => Promise<void>) => {
-  let app;
-  let state = 'start';
   const prepare = bootstrap()
     .then(nestApp => {
       return nestApp.init()
@@ -12,30 +10,8 @@ export const createHttpServer = (): ((req: NowRequest, res: NowResponse) => Prom
     });
 
   return async (req: NowRequest, res: NowResponse) => {
-    if (process.env.SURGIO_TESTING_STARTUP) {
-      switch (state) {
-        case 'start':
-          res.status(503).send('Under Maintenance');
-          state = 'preparing';
-          prepare.then(nextApp => {
-            app = nextApp;
-            state = 'ready';
-          });
-
-          break;
-        case 'preparing':
-          res.status(503).send('Under Maintenance');
-
-          break;
-        case 'ready':
-          app.getHttpAdapter().getInstance().server.emit('request', req, res);
-
-          break;
-      }
-    } else {
-      const nestApp = await prepare;
-      nestApp.getHttpAdapter().getInstance().server.emit('request', req, res);
-    }
+    const nestApp = await prepare;
+    nestApp.getHttpAdapter().getInstance().server.emit('request', req, res);
   };
 };
 
