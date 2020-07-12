680d54310d72e1ffba58e8458d7a2913120c3f48
Roy Li <me@dada.li>
2020-07-12 14:46:38 +0800

    chore: comment lambda functions

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -2,6 +2,7 @@ import 'source-map-support/register';
 import { ConfigService } from '@nestjs/config';
 import { NowRequest, NowResponse } from '@now/node/dist';
 import { createServer, Server } from 'http';
+
 import { bootstrap } from './bootstrap';
 
 export const createHttpServer = (): Server => {
@@ -37,5 +38,42 @@ export const startServer = (): Promise<Server> => {
       process.exit(1);
     });
 };
+
+// export const createAwsHandler = (): ((event: any, context: Context) => void) => {
+//   let lambdaProxy: Server;
+//   const prepare = bootstrap()
+//     .then(nestApp => {
+//       return nestApp.init();
+//     });
+//
+//   return (event: any, context: Context) => {
+//     if (!lambdaProxy) {
+//       prepare.then((nestApp) => {
+//         lambdaProxy = nestApp.getHttpServer();
+//         serverlessExpress.proxy(lambdaProxy, event, context);
+//       });
+//     } else {
+//       serverlessExpress.proxy(lambdaProxy, event, context);
+//     }
+//   };
+// };
+//
+// export const createServerlessHanlder = () => {
+//   let lambdaProxy: ServerlessHandler;
+//   const prepare = bootstrap()
+//     .then(nestApp => {
+//       return nestApp.init();
+//     });
+//
+//   return async (event: any, context: Context): Promise<APIGatewayProxyResult> => {
+//     if (!lambdaProxy) {
+//       const nestApp = await prepare;
+//       lambdaProxy = serverless(nestApp.getHttpServer());
+//       return await lambdaProxy(event, context);
+//     } else {
+//       return await lambdaProxy(event, context);
+//     }
+//   };
+// };
 
 export const bootstrapServer = bootstrap;
