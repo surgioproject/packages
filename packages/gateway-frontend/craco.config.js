0dc187d6cb3d4565affb7a7163e1f2d2b21ebab1
Roy Li <me@royli.dev>
2022-12-31 21:27:07 +0100

    chore: remove service worker

--- packages/gateway-frontend/craco.config.js
+++ packages/gateway-frontend/craco.config.js
@@ -1,19 +1,14 @@
 'use strict';
 
-const CracoWorkboxPlugin = require('craco-workbox');
-const { ESLINT_MODES } = require("@craco/craco");
+const { ESLINT_MODES } = require('@craco/craco');
 const pkg = require('./package.json');
 
 process.env.REACT_APP_VERSION = pkg.version;
 
 module.exports = {
   eslint: {
-    mode: ESLINT_MODES.file
+    mode: ESLINT_MODES.file,
   },
-  plugins: [
-    {
-      plugin: CracoWorkboxPlugin,
-    },
-  ],
+  plugins: [],
   style: {},
 };
