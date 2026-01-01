605a4dc74e04b263b117ff42e216dc06cc4afbb0
Roy Li <me@royli.dev>
2026-01-01 12:07:56 +0100

    chore(eslint-config-surgio): migrate to eslint v9 flat config format
    BREAKING CHANGE: Migrate eslint-config-surgio from legacy .eslintrc format to ESLint
    v9 flat config format. Add @eslint/js and globals as dependencies,
    and update peerDependencies to require eslint >=9.0.0.

--- packages/eslint-config-surgio/index.js
+++ packages/eslint-config-surgio/index.js
@@ -1,24 +1,29 @@
 'use strict';
 
-module.exports = {
-  extends: [
-    'eslint:recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
+const js = require('@eslint/js');
+const globals = require('globals');
+
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.es2020,
+        ...globals.node,
+        ...globals.commonjs,
+      },
+    },
+    rules: {
+      'array-bracket-spacing': 'off',
+      'comma-dangle': 'off',
+      'dot-notation': 'off',
+      'valid-jsdoc': 'off',
+      'no-unused-vars': 'off',
+      'no-mixed-spaces-and-tabs': 'off',
+      eqeqeq: ['error', 'allow-null'],
+    },
   },
-  env: {
-    es6: true,
-    node: true,
-    commonjs: true,
-  },
-  rules: {
-    'array-bracket-spacing': 'off',
-    'comma-dangle': 'off',
-    'dot-notation': 'off',
-    'valid-jsdoc': 'off',
-    'no-unused-vars': 'off',
-    'no-mixed-spaces-and-tabs': 'off',
-    eqeqeq: ['error', 'allow-null']
-  },
-};
+];
